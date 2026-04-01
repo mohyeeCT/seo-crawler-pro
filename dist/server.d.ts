6d86bc3751ee221c23bd26283d18e8d5ef4e1f40
mohyeeCT <76063943+mohyeeCT@users.noreply.github.com>
2026-04-02 00:29:53 +0200

    Add dist/server.d.ts

---
+++ dist/server.d.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+interface ServerOptions {
+    directory: string;
+    port: number;
+}
+/**
+ * Simple static file server for viewing SEO reports
+ */
+export declare function startServer(options: ServerOptions): void;
+export {};
+//# sourceMappingURL=server.d.ts.map
