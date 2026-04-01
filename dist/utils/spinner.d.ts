a223ca2d1494e67b0a55a49b88ed4caecdce0bea
mohyeeCT <76063943+mohyeeCT@users.noreply.github.com>
2026-04-02 00:30:25 +0200

    Add dist/utils/spinner.d.ts

---
+++ dist/utils/spinner.d.ts
@@ -0,0 +1,9 @@
+export declare function createSpinner(initialText: string): {
+    start(text?: string): /*elided*/ any;
+    succeed(text?: string): /*elided*/ any;
+    warn(text?: string): /*elided*/ any;
+    fail(text?: string): /*elided*/ any;
+    text: string;
+    textUpdate: string;
+};
+//# sourceMappingURL=spinner.d.ts.map
